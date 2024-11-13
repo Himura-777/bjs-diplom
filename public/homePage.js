2f16f9e1ef4a5ff43964777f3282b4dd03e29841
Tuan Anh <tp.anhh7@gmail.com>
2024-11-13 19:01:04 +0300

    Second departure

--- public/homePage.js
+++ public/homePage.js
@@ -1,3 +1,5 @@
+const { response } = require('express');
+
 // Выход из личного кабинета
 const logOut = new LogoutButton();
 logOut.action(() => {
@@ -27,3 +29,35 @@ function getCurrentRates() {
 }
 getCurrentRates();
 setInterval(getCurrentRates, 60000);
+
+// Операции с деньгами
+const moneyManger = new MoneyManager();
+moneyManger.addMoneyCallback = function () {
+	addMoneyAction();
+	if (response.success === true) {
+		showProfile(response.data);
+		setMessage('Пополнение прошло успешно.');
+	} else {
+		setMessage(`Ошибка пополнения: ${response.error}`);
+	}
+};
+
+moneyManger.conversionMoneyCallback = function () {
+	convertMoney();
+	if (response.success === true) {
+		showProfile(response.data);
+		setMessage('Конвертирование прошло успешно.');
+	} else {
+		setMessage(`Ошибка конвертирования: ${response.error}`);
+	}
+};
+
+moneyManger.sendMoneyCallback = function () {
+	transferMoney();
+	if (response.success === true) {
+		showProfile(response.data);
+		setMessage('Перевод прошёл успешно.');
+	} else {
+		setMessage(`Ошибка перевода: ${response.error}`);
+	}
+};
